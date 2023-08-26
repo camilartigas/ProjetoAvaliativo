9898d2d0c3b501aee603d6003f154ac7ed6fba30
Camila <camiart@gmail.com>
2023-08-25 22:26:05 -0300

    create cadastro de ofertas

--- src/pages/Ofertas.jsx
+++ src/pages/Ofertas.jsx
@@ -1,20 +1,58 @@
-import { Header } from "../components/header";
+import React, { useState, useEffect } from 'react';
+import { Header } from '../components/header';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-function Ofertas(){
-    return(
-        <>
-        <Header/>
-        <p>Uma implementação de página com funcionalidade extra criada por você. Ex
-a. Página de cadastro de usuário para ser utilizada no login
-b. Página de esqueceu sua senha
-c. Página de FAQ
-d. Entre outras funcionalidades
+function Ofertas() {
+  const [medications, setMedications] = useState([]);
+  
+  useEffect(() => {
+    const existingMedications = JSON.parse(localStorage.getItem('medications')) || [];
+    setMedications(existingMedications);
+  }, []);
+  
+  const handlePriceChange = (medicationName, newPrice) => {
+    const updatedMedications = medications.map((medication) => {
+      if (medication.nomeMedicamento === medicationName) {
+        return { ...medication, preco: newPrice };
+      }
+      return medication;
+    });
 
-verificar header e footer default para todas as pg.
-        </p>
-      
-        </>
-    )
+    setMedications(updatedMedications);
+    localStorage.setItem('medications', JSON.stringify(updatedMedications));
+  };
+
+  return (
+    <>
+      <Header />
+      <div>
+        <Box sx={{ p: 3 }}>
+          <Typography variant="h5">Cadastro de Ofertas</Typography>
+        </Box>
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '16px', padding: '16px' }}>
+          {medications.map((medication) => (
+            <div key={medication.nomeMedicamento}>
+              <Typography variant="h6">{medication.nomeMedicamento}</Typography>
+              <Typography variant="body1">Preço: {medication.preco}</Typography>
+              <TextField
+                label="Novo Preço"
+                variant="outlined"
+                fullWidth
+                type="text"
+                defaultValue={medication.preco}
+                onChange={(event) =>
+                  handlePriceChange(medication.nomeMedicamento, event.target.value)
+                }
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export { Ofertas };
