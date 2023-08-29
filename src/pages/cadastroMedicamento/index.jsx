ea89d9044eea6545bfca0ea009d09a427f6d806e
Camila <camiart@gmail.com>
2023-08-28 21:40:30 -0300

    limpa ean

--- src/pages/cadastroMedicamento/index.jsx
+++ src/pages/cadastroMedicamento/index.jsx
@@ -75,9 +75,9 @@ function CadastroMedicamentos() {
             localStorage.setItem('medications', JSON.stringify(existingMedications));
 
             // Limpa os campos após o cadastro
+            setEan('');
             event.target.nomeMedicamento.value = '';
             event.target.nomeLaboratorio.value = '';
-            event.target.ean.value = '';
             event.target.dosagem.value = '';
             event.target.medida.value = 'mg'; // Define um valor padrão para a medida
             event.target.preco.value = 'R$ ';
